1d959c698138d3ffc3274ecb1f3edfb020ccc855
stevenBhub <stevenbedoll@gmail.com>
2025-02-27 11:04:36 -0700

    Create main_base.cpp

---
+++ main_base.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : Steven Bedoll
+// Author      : Steven Bedoll
+// Version     : A
+// Copyright   : Your copyright notice
+// Input       : Basic searching examples
+// Process     :
+// OutPut      :
+//=============================================+++++++++======================
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <time.h>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
